0faf2e0b9aacd9db35b5a227b11742233c36e79f
Mr_Staf <ben82160@gmail.com>
2021-05-18 12:04:33 +0200

    Unit test + Main project

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c67d28d-573c-4c3d-a861-4deb3c80bdcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var math = new GitUnitTestDemo.Math(5, 2);
+            var math2 = new GitUnitTestDemo.Math(1234, 0);
+
+            Assert.AreEqual(7, math.Add());
+            Assert.AreEqual(3, math.Subtract());
+            Assert.AreEqual(10, math.Multiply());
+            Assert.AreEqual(5, math.SumDigits());
+            Assert.AreEqual("101", math.Binary());
+            Assert.AreEqual(2.5, math.Divide());
+                   
+            Assert.AreEqual(1234, math2.Add());
+            Assert.AreEqual(1234, math2.Subtract());
+            Assert.AreEqual(0, math2.Multiply());
+            Assert.AreEqual(10, math2.SumDigits());
+            Assert.AreEqual("10011010010", math2.Binary());
+            Assert.AreEqual(Single.PositiveInfinity, math2.Divide());
+        }
+    }
+}
